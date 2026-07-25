96486594dfdef1c8a15fe6fdc6ecbf662f11a5d5
Aniket <aniket@Anikets-MacBook-Air.local>
2026-07-25 16:34:14 +0530

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Replace with your GitHub repository name
+  base: '/CoreFlex-Physio/',
+
   build: {
     rollupOptions: {
       output: {
